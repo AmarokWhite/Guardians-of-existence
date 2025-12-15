6480dff24c99bc9b9e8a09752994084541ff65f0
Amarok White <pribytkovsergey7@gmail.com>
2025-12-16 02:19:19 +0300

    Delete js directory

--- js/core/constants.js
+++
@@ -1,8 +0,0 @@
-export const TOWER_DATA = {
-    0: { cost: 100, range: 100, dmg: 20, cd: 50, name: "Солнечная Вспышка", effect: null },
-    1: { cost: 150, range: 160, dmg: 8, cd: 40, name: "Стазис-Луч", effect: 'slow' },
-    2: { cost: 220, range: 120, dmg: 15, cd: 45, name: "Грави-Колодец", effect: 'splash' },
-    3: { cost: 250, range: 300, dmg: 40, cd: 90, name: "Снайпер-Дрон", effect: 'far' },
-    4: { cost: 300, range: 140, dmg: 25, cd: 55, name: "Бронебой", effect: 'pierce' },
-    5: { cost: 200, range: 130, dmg: 10, cd: 35, name: "Глитч-Турель", effect: 'shotgun' }
-};
